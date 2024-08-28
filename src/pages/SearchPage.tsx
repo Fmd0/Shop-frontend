56be166a3e47916bf1b51310e628e7cb1482f218
ChenNan <fmd2024@163.com>
2024-08-28 21:31:38 +0800

    fix homepage and searchpage smallnavbar auto move

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -7,9 +7,46 @@ import SmallBottomSideBar from "../components/common/SmallBottomSideBar.tsx";
 import SmallLogoutModal from "../components/common/SmallLogoutModal.tsx";
 import AddToLikeNotificationModal from "../components/common/AddToLikeNotificationModal.tsx";
 import RecentlyViewedListCartPage from "../components/cartPage/RecentlyViewedListCartPage.tsx";
+import useUserInfoStore from "../hooks/useUserInfoStore.ts";
+import {useEffect} from "react";
 
 
 const SearchPage = () => {
+
+    const { setShowSmallNavBar} = useUserInfoStore();
+
+    useEffect(() => {
+        let isThrottled = false;
+        let scrollY = 0;
+        const handleScroll = () => {
+            if(isThrottled) {
+                return;
+            }
+            isThrottled = true;
+            setTimeout(() => {
+                isThrottled = false;
+            }, 30)
+            // console.log("window.scrollY", window.scrollY);
+            // console.log(window.scrollY - scrollY);
+            // console.log(window.document.documentElement.scrollHeight - window.innerHeight);
+            if(window.scrollY>0 && window.scrollY<window.document.documentElement.scrollHeight - window.innerHeight) {
+                if(window.scrollY - scrollY > 10) {
+                    setShowSmallNavBar(false);
+                }
+                else if(window.scrollY - scrollY < -10) {
+                    setShowSmallNavBar(true);
+                }
+            }
+            scrollY = window.scrollY;
+        }
+        window.addEventListener("scroll",handleScroll);
+
+        return () => {
+            window.removeEventListener("wheel", handleScroll);
+        }
+    }, []);
+
+
     return (
         <>
             <NavBarSearchPage />
